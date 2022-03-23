2a4e9ba3e01176f7dfd9320d4b68eb53b07e974e
Hairy Monkee <hairymonkee@protonmail.com>
2022-03-23 20:05:38 +0000

    Added wallet connection

--- components/layout/header/menu/index.tsx
+++ components/layout/header/menu/index.tsx
@@ -1,6 +1,8 @@
 import Classes from './src/menu.module.scss'
 import NavBar from './navbar'
 import Button from '@/components/button'
+import { ethers } from 'ethers'
+import { useEffect, useState } from 'react'
 
 /**
  * Interfaces
@@ -10,11 +12,34 @@ interface Buttons {
     url: string
 }
 
+// Skip typechecking on the window object
+declare let window: any
+
 /**
  * Menu component
  */
 const Menu = () => {
     // local states
+    const [auth, setAuth] = useState<boolean>(false)
+
+    // Connect to wallet
+    const connect = async () => {
+        // Await connection
+        await window.ethereum.enable()
+
+        // Set auth to true
+        setAuth(true)
+    }
+
+    useEffect(() => {
+        // If ethereum exists in the window object
+        if (window.ethereum) {
+            // Initialize ethers.js
+            const provider = new ethers.providers.Web3Provider(window.ethereum)
+            // Ask User permission to connect to Metamask
+            connect()
+        }
+    }, [])
 
     // Buttons array
     const buttons: Buttons[] = [
@@ -45,11 +70,20 @@ const Menu = () => {
             <NavBar buttons={buttons} />
 
             <div className={Classes.wallet}>
-                <Button
-                    name="Connect Wallet"
-                    variant="light"
-                    icon={<img src="/images/heart.png" alt="heart" />}
-                />
+                {auth ? (
+                    <Button
+                        name="Wallet Connected"
+                        variant="light"
+                        icon={<img src="/images/heart.png" alt="heart" />}
+                    />
+                ) : (
+                    <Button
+                        name="Connect Wallet"
+                        variant="light"
+                        icon={<img src="/images/heart.png" alt="heart" />}
+                        onClick={connect}
+                    />
+                )}
             </div>
         </div>
     )
