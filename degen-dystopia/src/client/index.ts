fea79b656866f301d495901b05f9a8abdfc0e757
Scott Weiss <sdw35@cornell.edu>
2022-03-30 21:55:35 -0400

    add dd game entrypoint

---
+++ degen-dystopia/src/client/index.ts
@@ -0,0 +1,23 @@
+import Start from "./game"
+
+window.addEventListener('load', () => {
+    rungame()    
+})
+
+export function rungame() {
+    // initialize dependencies and inject into ui
+
+    // connect to server
+
+    // connect to contract
+
+    // create UI and inject
+    Start()
+}
+
+// export default { rungame: rungame }
+export const test = 'test'
+
+export const runGame = () => {
+    console.log('running game...')
+}
